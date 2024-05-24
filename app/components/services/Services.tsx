1a9d722907572d87fe85cd162d784359f3b87345
arunkumar0398 <32316065+arunkumar0398@users.noreply.github.com>
2024-05-24 22:23:41 +0530

    update service section, used swiper in sliderData
    - added styling for service

---
+++ app/components/services/Services.tsx
@@ -0,0 +1,114 @@
+"use client";
+import React from "react";
+import styles from "./service.module.css";
+import './service.module.css';
+import { Autoplay} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { sliderData } from "../../utils/sliderData";
+import "aos/dist/aos";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const Services = () => {
+
+	React.useEffect(() => {
+		AOS.init();
+    	AOS.refresh();
+	}, []);
+
+	return (
+		<div className={styles.services}>
+			<div className={styles.service_wrapper}>
+				<div className={styles.services_left} 
+					data-aos="fade-right"
+					data-aos-duration="1200">
+					<p className={styles.services_subtitle}>Services</p>
+					<h2 className={styles.services_title}>
+						Our top value categories for you
+					</h2>
+				</div>
+				<div className={styles.services_right}>
+					<Swiper
+						slidesPerView={2.5}
+						spaceBetween={21}
+						autoplay={true}
+						loop={true}
+						modules={[Autoplay]}
+						className={styles.services_slider}
+						centeredSlides={true}
+						breakpoints={{
+							320: {
+								slidesPerView: 2,
+								spaceBetween: 20,
+							},
+
+							480: {
+								slidesPerView: 2,
+								spaceBetween: 30,
+							},
+
+							545: {
+								slidesPerView: 2,
+								spaceBetween: 20,
+							},
+
+							640: {
+								slidesPerView: 2,
+								spaceBetween: 20,
+							},
+
+							768: {
+								slidesPerView: 2,
+								spaceBetween: 20,
+							},
+
+							1024: {
+								slidesPerView: 2,
+								spaceBetween: 30,
+							},
+
+							1280: {
+								slidesPerView: 3,
+								spaceBetween: 21,
+							},
+
+							1920: {
+								slidesPerView: 2,
+								spaceBetween: 20,
+							},
+						}}
+					>
+						{sliderData.map((slide) => {
+							return (
+								<SwiperSlide
+									key={slide.id}
+									className={styles.services_card}
+								>
+									<div className={styles.services_icon}>
+										<img
+											src={`/assets/${slide.imageSrc}`}
+											alt={slide.title}
+											className={styles.services_image}
+										/>
+									</div>
+
+									<h3 className={styles.card_title}>
+										{slide.title}
+									</h3>
+									<p className={styles.card_subtitle}>
+										{slide.subTitle}
+									</p>
+								</SwiperSlide>
+							);
+						})}
+					</Swiper>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Services;
---
+++ app/utils/sliderData.ts
@@ -0,0 +1,26 @@
+export const sliderData = [
+	{
+		id: 1,
+		title: "Best Tour Guide",
+		imageSrc: "destination.png",
+		subTitle: "What looked like a small patch of purple grass, above five feet.",
+	},
+	{
+		id: 2,
+		title: "Easy Booking",
+		imageSrc: "booking.png",
+		subTitle: "Square, was moving across the sand in their direction.",
+	},
+	{
+		id: 3,
+		title: "Best Tour Guide",
+		imageSrc: "destination.png",
+		subTitle: "What looked like a small patch of purple grass, above five feet.",
+	},
+	{
+		id: 4,
+		title: "Easy Booking",
+		imageSrc: "booking.png",
+		subTitle: "Square, was moving across the sand in their direction.",
+	},
+];
